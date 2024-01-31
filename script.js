7b232a219f04ab9f652979fd56870bc2d6c45bb8
Spencer Johnson <123844048+SpencerJCode@users.noreply.github.com>
2024-01-31 16:44:46 -0500

    Psalm 1 and player uploaded

---
+++ script.js
@@ -0,0 +1,21 @@
+function goToPsalm() {
+    var e = document.getElementById("psalm_select");
+    var psalm = e.value;
+    window.location.replace(`/music/psalms/${psalm}.html`);
+}
+
+function playPsalm() {
+    var button = document.getElementById("player");
+    var audio;
+    if (button.innerHTML == "PLAY") {
+        var e = document.getElementById("psalm_title");
+        e = e.innerHTML;
+        var psalmNumber = e.substring(6,e.length);
+        console.log(psalmNumber);
+        audio = new Audio(`/assets/musicfiles/Psalm${psalmNumber}.mp3`);
+        audio.play();
+        button.innerHTML = "STOP"
+    } else {
+        location.reload();
+    }
+}
