ba8085765eed72a042e45c5146ab29c7be1e579c
SpencerJCode <spencertjohnson@proton.me>
2026-09-05 16:29:23 -0400

    Adding 3 psalms

--- goshenWebEngine.js
+++ goshenWebEngine.js
@@ -165,7 +165,7 @@ const psalmsEngine = class {
     constructor() {
         this.selectedPsalm = 1;
         this.audioPath = `/assets/musicfiles/Psalm*.mp3`;
-        this.PsalmsOptions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,20,21,22,23,24,25,27,28,29,30,31,32,33,34,35,36,37,40,66,75,90,98,147];
+        this.PsalmsOptions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,20,21,22,23,24,25,27,28,29,30,31,32,33,34,35,36,37,40,50,66,75,84,90,98,147,149];
         this.audio = new Audio(`/assets/musicfiles/Psalm${1}.mp3`);
         this.button = null;
     }
